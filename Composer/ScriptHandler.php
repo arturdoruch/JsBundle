7d37669102f1dbb87b7e9647bfd151812b41eb2d
Artur Doruch <arturdoruch@interia.pl>
2019-12-11 03:04:51 +0100

    Update dependency package arturdoruch/js to version 1.1.0

--- Composer/ScriptHandler.php
+++ Composer/ScriptHandler.php
@@ -20,7 +20,7 @@ class ScriptHandler
      */
     public static function installJs(Event $event)
     {
-        $version = '1.0.9';
+        $version = '1.1.0';
         $targetDir = __DIR__ . '/../Resources/public/js';
 
         if ($version === $installedVersion = self::getInstalledVersion($targetDir)) {
